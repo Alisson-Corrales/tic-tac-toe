f5a4f476a319942c6bb8a8e8659aad70d12770fe
Alisson Corrales <alisson.corrales24@gmail.com>
2020-11-29 22:38:13 -0700

    made the file AC 11/29/2020

---
+++ script.js
@@ -0,0 +1,48 @@
+"use strict";
+
+
+let player = '';
+let turns = 0;
+let playerX = 0;
+let playerO = 0;
+
+document.getElementById("redo").onclick = function () {
+    resetGame();
+}
+
+
+//when the player wins this happens
+function playerWins() {
+
+}
+
+//when the player clicks one of the boxes, this changes turns
+function playerTurns() {
+    for (let boxes = 0; boxes > ) {
+
+    }
+}
+
+//when the player clicks one of the boxes, this marks the box down
+let playerChoice = function () {
+    let elemlist = document.getElementsByClassName(ticTacToeChart);
+    for(let )
+    if (playerTurns == 'X') {
+        document.getElementById().textContent = "X"
+    }
+    if (playerTurns == 'O') {
+        document.getElementById().textContent = "O"
+    }
+    return
+}
+
+//resets game
+function resetGame() {
+    let player = '';
+    let turns = 0;
+    let playerX = 0;
+    let playerO = 0;
+
+    let elemList = document.getElementsByClassName("startover");
+    for (let)
+}
