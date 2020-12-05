ac718ca16873c322b153814ea17aa4fa8d197209
Alisson Corrales <alisson.corrales24@gmail.com>
2020-12-04 22:35:46 -0700

    still can't make the win function to work. got it set up though AC 12/4/2020

--- script.js
+++ script.js
@@ -38,24 +38,90 @@ function playerChoice(num) {
 
 //this lets a player win if they get 3 boxes w/ the same letter in a row
 function playerWins() {
-    //boxes = ['0', `1`, `2`, `3`, `4`, `5`, `6`, `7`, `8`, `9`]; for reference
-    
     //makes points go up by depending who wins
     playerX++;
     playerY++;
-    
+
+    //if X player wins
     if (boxes[1] == 'X' && boxes[2] == 'X' && boxes[3] == 'X') {
-        console.log('you win!');
+        alert('you win!');
         document.getElementById('redo').textContent = "Restart?";
         document.getElementById('playerX').textContent = `Player X wins - ${playerX}`;
     }
     if (boxes[4] == 'X' && boxes[5] == 'X' && boxes[6] == 'X') {
-        console.log('you win!');
+        alert('you win!');
         document.getElementById('redo').textContent = "Restart?";
         document.getElementById('playerX').textContent = `Player X wins - ${playerX}`;
     }
     if (boxes[7] == 'X' && boxes[8] == 'X' && boxes[9] == 'X') {
-        console.log('you win!');
+        alert('you win!');
+        document.getElementById('redo').textContent = "Restart?";
+        document.getElementById('playerX').textContent = `Player X wins - ${playerX}`;
+    }
+    if (boxes[1] == 'X' && boxes[4] == 'X' && boxes[7] == 'X') {
+        alert('you win!');
+        document.getElementById('redo').textContent = "Restart?";
+        document.getElementById('playerX').textContent = `Player X wins - ${playerX}`;
+    }
+    if (boxes[2] == 'X' && boxes[5] == 'X' && boxes[8] == 'X') {
+        alert('you win!');
+        document.getElementById('redo').textContent = "Restart?";
+        document.getElementById('playerX').textContent = `Player X wins - ${playerX}`;
+    }
+    if (boxes[3] == 'X' && boxes[6] == 'X' && boxes[9] == 'X') {
+        alert('you win!');
+        document.getElementById('redo').textContent = "Restart?";
+        document.getElementById('playerX').textContent = `Player X wins - ${playerX}`;
+    }
+    if (boxes[1] == 'X' && boxes[5] == 'X' && boxes[9] == 'X') {
+        alert('you win!');
+        document.getElementById('redo').textContent = "Restart?";
+        document.getElementById('playerX').textContent = `Player X wins - ${playerX}`;
+    }
+    if (boxes[3] == 'X' && boxes[5] == 'X' && boxes[7] == 'X') {
+        alert('you win!');
+        document.getElementById('redo').textContent = "Restart?";
+        document.getElementById('playerX').textContent = `Player X wins - ${playerX}`;
+    }
+
+    //if O player wins
+    if (boxes[1] == 'O' && boxes[2] == 'O' && boxes[3] == 'O') {
+        alert('you win!');
+        document.getElementById('redo').textContent = "Restart?";
+        document.getElementById('playerX').textContent = `Player X wins - ${playerX}`;
+    }
+    if (boxes[4] == 'O' && boxes[5] == 'O' && boxes[6] == 'O') {
+        alert('you win!');
+        document.getElementById('redo').textContent = "Restart?";
+        document.getElementById('playerX').textContent = `Player X wins - ${playerX}`;
+    }
+    if (boxes[7] == 'O' && boxes[8] == 'O' && boxes[9] == 'O') {
+        alert('you win!');
+        document.getElementById('redo').textContent = "Restart?";
+        document.getElementById('playerX').textContent = `Player X wins - ${playerX}`;
+    }
+    if (boxes[1] == 'O' && boxes[4] == 'O' && boxes[7] == 'O') {
+        alert('you win!');
+        document.getElementById('redo').textContent = "Restart?";
+        document.getElementById('playerX').textContent = `Player X wins - ${playerX}`;
+    }
+    if (boxes[2] == 'O' && boxes[5] == 'O' && boxes[8] == 'O') {
+        alert('you win!');
+        document.getElementById('redo').textContent = "Restart?";
+        document.getElementById('playerX').textContent = `Player X wins - ${playerX}`;
+    }
+    if (boxes[3] == 'O' && boxes[6] == 'O' && boxes[9] == 'O') {
+        alert('you win!');
+        document.getElementById('redo').textContent = "Restart?";
+        document.getElementById('playerX').textContent = `Player X wins - ${playerX}`;
+    }
+    if (boxes[1] == 'O' && boxes[5] == 'O' && boxes[9] == 'O') {
+        alert('you win!');
+        document.getElementById('redo').textContent = "Restart?";
+        document.getElementById('playerX').textContent = `Player X wins - ${playerX}`;
+    }
+    if (boxes[3] == 'O' && boxes[5] == 'O' && boxes[7] == 'O') {
+        alert('you win!');
         document.getElementById('redo').textContent = "Restart?";
         document.getElementById('playerX').textContent = `Player X wins - ${playerX}`;
     }
@@ -65,7 +131,4 @@ function playerWins() {
 function resetGame() {
     player = 'X';
     numOfTurns = 0;
-    playerX = 0;
-    playerO = 0;
-    boxes = [`0`, `1`, `2`, `3`, `4`, `5`, `6`, `7`, `8`, `9`];
 }
